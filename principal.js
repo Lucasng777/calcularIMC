cff94142778b8ef35aa6abf60640fe6ef57e7afb
Lucasng777 <163145504+Lucasng777@users.noreply.github.com>
2024-09-30 18:50:58 -0300

    acabei

---
+++ principal.js
@@ -0,0 +1,12 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista"
+
+var pacientes = document.querySelectorAll('.paciente');
+for (let contador = 0; contador < pacientes.length; contador++){
+    var dadosPacientes = pacientes[contador];
+    var peso = dadosPacientes.querySelector('.info-peso').textContent;
+    var altura = dadosPaciente.querySelector('.info-altura').textContent;
+    var imc = peso / (altura*altura);
+    dadso
+
+}
